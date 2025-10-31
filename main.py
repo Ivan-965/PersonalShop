647356de9e103e0db487fbb8754b35c0d030df25
Ivan-965 <skosiv13@gmail.com>
2025-10-31 20:48:04 +0300

    Command start

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from handlers import h01_start
+from config import TOKEN
 
+
+bot = Bot(token=TOKEN)
+
+dp = Dispatcher()
+
+dp.include_router(h01_start.router)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
