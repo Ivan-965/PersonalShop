f0106f6356ee116ce18a17fbceef9d224cf4797d
Ivan-965 <skosiv13@gmail.com>
2025-11-28 21:18:32 +0300

    Создание таблиц для базы данных

--- database/models/users.py
+++ database/models/users.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database.base import Base
 
 class Users(Base):
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(70))
