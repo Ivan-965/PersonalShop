4159b976e0388a96076e4961a20d6a0c8422d23f
Ivan-965 <skosiv13@gmail.com>
2026-01-23 20:31:52 +0300

    Исправил ошибку и закончил генерацию категорий меню.

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -1,21 +1,23 @@
 from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+from database.utils import db_get_all_category, db_get_finally_price
+
 """Модуль с функцими для создания inline клавиатур"""
 
 
 def generate_category_menu(chat_id):
-    """Генерация меню категорий"""
-    categories = db_get_all_categories()
+    categories = db_get_all_category()
     total_price = db_get_finally_price(chat_id)
 
     builder = InlineKeyboardBuilder()
     builder.button(
-        text=f"Корзина заказа ({total_price if total_price else 0}рублей)",
-        сallback_data="Корзина заказа"
+        text=f'Корзина заказа ({total_price if total_price else 0} рублей)',
+        callback_data='Корзина заказа'
     )
 
     [builder.button(text=category.category_name, callback_data=f'category_{category.id}')
-    for category in categories]
+     for category in categories]
+
     builder.adjust(1, 2)
-    return builder.as_markup(resize_keyboard=True)
+    return builder.as_markup()
