ad3918fe4a4cfae8268d0698751ceade0ebdd15c
Ivan-965 <skosiv13@gmail.com>
2026-03-06 20:33:22 +0300

    Начало работы над изменением и добавлением в корзине пользователя

--- database/utils.py
+++ database/utils.py
@@ -92,3 +92,36 @@ def db_get_product_by_id(product_id):
     with get_session() as session:
         query = select(Products).where(Products.id == product_id)
         return session.scalar(query)
+
+
+def db_get_user_cart(chat_id):
+    """Получение корзины пользователя по ID корзины"""
+
+    with get_session() as session:
+        query = select(Carts).join(Users, Carts.user_id == Users.id).where(Users.telegram == chat_id)
+        return session.scalar(query)
+
+
+def db_add_or_update_item(cart_id, product_id, product_name, product_price, increment: int = 0):
+    """Функция добавляет или обновит количество существующего товара"""
+    try:
+        with get_session() as session:
+            item = (
+                session.query(FinallyCarts).filter_by(cart_id=cart_id, product_id=product_id).first()
+            )
+            if item:
+                if increment != 0:
+                    item.quantity = max(1, item.quantity + increment)
+
+            else:
+                qty = 1 if increment <= 0 else increment
+                item = FinallyCarts(
+                    cart_id=cart_id,
+                    product_id=product_id,
+                    product_name=product_name,
+                    quantity=qty,
+                    final_price=0,
+                )
+                session.add(item)
+
+            item.final_price = item.quantity * product_price
--- handlers/h06_product_detail.py
+++ handlers/h06_product_detail.py
@@ -1,10 +1,12 @@
 from aiogram import Router, F, Bot
 from aiogram.types import FSInputFile, CallbackQuery
 
-from database.utils import db_get_product_by_id
+from bot_utils.message_uitils import text_for_caption
+from database.utils import db_get_product_by_id, db_get_user_cart, db_add_or_update_item
 from keyboards.inline import generate_category_menu
 
 router = Router()
+
 
 @router.callback_query(F.data.startswith("product_view"))
 async def show_product_detail(callback: CallbackQuery, bot: Bot):
@@ -12,7 +14,36 @@ async def show_product_detail(callback: CallbackQuery, bot: Bot):
     chat_id = callback.message.chat.id
     message_id = callback.message.message_id
 
-    await bot.delete_message(chat_id= chat_id, message_id= message_id)
+    await bot.delete_message(chat_id=chat_id, message_id=message_id)
 
     product_id = int(callback.data.split("_")[-1])
     product = db_get_product_by_id(product_id)
+    user_cart = db_get_user_cart(chat_id)
+
+    if user_cart:
+        db_add_or_update_item(
+            cart_id = user_cart.id,
+            product_id = product.id,
+            product_name = product.product_name,
+            product_price = product.price,
+            increment = 1
+        )
+
+        caption = text_for_caption(
+            name = product.product_name,
+            description = product.description,
+            base_price = float(product.price),
+        )
+
+        product_image = FSInputFile(path = product.image)
+
+        await bot.send_photo(
+            chat_id=chat_id,
+            photo=product_image,
+            caption = caption,
+            parse_mode = "HTML",
+            reply_markup= quantity_cart_controls()
+        )
+
+    else:
+         await ask_for_phone(chat_id, bot)
